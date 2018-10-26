d41926b80a46d5653264f2d8913973f1f12fa9ec
Unknown <markuswiebe@web.de>
2018-10-26 04:51:46 +0200

    Express Hello World

---
+++ index.js
@@ -0,0 +1,12 @@
+var config = require('./config.json');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(5000, function() {
+    console.log('Server listening on port 5000');
+});
